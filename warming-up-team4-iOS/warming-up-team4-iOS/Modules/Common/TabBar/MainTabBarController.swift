c83c8203d96f0e17d03fd818ff8b5a5a49549119
jeong jin ho <yyyqor@gmail.com>
2019-10-19 02:05:35 +0900

    프로젝트 폴더링 및 앱 빌드 환경 세팅

---
+++ warming-up-team4-iOS/warming-up-team4-iOS/Modules/Common/TabBar/MainTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  MainTabBarController.swift
+//  warming-up-team4-iOS
+//
+//  Created by 진호놀이터 on 2019/10/19.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
