db1260c0c4a96de979fdfdf734f61ce4983f7c9f
jeong jin ho <yyyqor@gmail.com>
2019-10-19 17:03:46 +0900

    샘플 예제 프로젝트 추가

---
+++ BarterBetter/BarterBetter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BarterBetter
+//
+//  Created by 진호놀이터 on 2019/10/19.
+//  Copyright © 2019 진호놀이터. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
