c67830f7fc21f450233baa031753f4976954b627
jeong jin ho <yyyqor@gmail.com>
2019-10-21 21:57:16 +0900

    [#4] UI 개발 완료

---
+++ warming-up-team4-iOS/warming-up-team4-iOS/Modules/[01]Feed/[01-01]FeedMain/View/FeedView.swift
@@ -0,0 +1,89 @@
+//
+//  FeedView.swift
+//  warming-up-team4-iOS
+//
+//  Created by 진호놀이터 on 2019/10/20.
+//  Copyright © 2019 user. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+struct FeedView: View {
+    @State var users = ["Munon", "Gomin", "Durup"]
+    let height: CGFloat = 100.0
+    var body: some View {
+       
+        VStack {
+            appTitle
+            content
+        }
+    }
+    var content: some View {
+        List {
+            //ScrollView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("빵미미")
+                        .foregroundColor(Color("app_point_color"))
+                        .font(Font.custom("NanumSquareOTF_acR", size: 32))
+                    Text("님,")
+                        .font(Font.custom("NanumSquareOTF_acR", size: 32))
+                }
+                Text("오늘 올라온 상품이에요.")
+                    .font(Font.custom("NanumSquareOTF_acR", size: 32))
+                
+                Text("서울대학교")
+                    .foregroundColor(Color("app_point_color"))
+                    .font(Font.custom("NanumSquareOTF_acR", size: 10))
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topTrailing)
+            }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+            
+    
+            ForEach(users, id:  \.self) { user in
+                FeedCellView()
+            }
+        }
+    }
+    
+    var appTitle: some View {
+        Rectangle()
+        .fill(Color.white.opacity(1))
+        .frame( height: 50)
+        
+    }
+}
+//.font(Font.custom("Montserrat-Bold", size: 60.0))
+
+struct FeedCellView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Image("feed_image")
+                .resizable()
+                .frame(height: 300)
+                .clipped()
+            Text("베드트레이 교환 원해요")
+                .font(Font.custom("NanumSquareOTF_acR", size: 30))
+                
+            Text("23명이 교환원해요")
+                .foregroundColor(Color("app_point_color"))
+                .font(Font.custom("NanumSquareOTF_acR", size: 14))
+            Text("침대에서 정말 사용하기 편해요. 색상도 이쁘고 원목이라..")
+                .foregroundColor(Color("app_list_cotent_color"))
+                .font(Font.custom("NanumSquareOTF_acR", size: 14))
+            
+            
+        }.padding(.leading, 20)
+        .padding(.bottom, 20)
+            .padding(.trailing, -20)
+            
+        
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
+
+
